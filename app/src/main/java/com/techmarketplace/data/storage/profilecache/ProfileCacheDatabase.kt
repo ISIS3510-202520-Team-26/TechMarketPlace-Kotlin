ae219052d6a80099a2caa56e5e3ac8d167d01aaa
cdcp2 <c.casadiego@uniandes.edu.co>
2025-12-03 19:26:38 -0500

    fix for merge issues

--- app/src/main/java/com/techmarketplace/data/storage/profilecache/ProfileCacheDatabase.kt
+++ app/src/main/java/com/techmarketplace/data/storage/profilecache/ProfileCacheDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [UserListingEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class ProfileCacheDatabase : RoomDatabase() {
